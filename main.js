d344012bcd2eb6ff7b831b379298aeb5266903c3
abderahmenabdou <ettaharmammar@outlook.com>
2024-08-14 16:38:01 +0100

    Update

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-function dropDown(){
-    let dropDown = document.getElementById("dropDown");
-    let one = document.getElementById("one")
-    dropDown.classList.toggle("drop")
-}
-function dropDown2(){
-    let dropDown2 = document.getElementById("dropDown2");
-    dropDown2.classList.toggle("drop")
-}
+document.addEventListener("DOMContentLoaded", function () {
+  let arrows = document.querySelectorAll(".arrow");
+  arrows.forEach(function (arrow) {
+    arrow.addEventListener("click", function () {
+      let dropDown = this.querySelector(".links");
 
-function menuBar(){
-    let open = document.getElementById("open")
-    let closed = document.getElementById("closed")
-    let menu = document.getElementById("menu")
-    
-    open.classList.toggle("open")
-    closed.classList.toggle("closed")
-    menu.classList.toggle("ctn")
+      dropDown.classList.toggle("drop");
+
+      let imgElement = this.querySelector("img");
+      let src = imgElement.getAttribute("src");
+
+      src.includes("down")
+        ? (imgElement.src = src.replace("down", "up"))
+        : (imgElement.src = src.replace("up", "down"));
+
+      // if(src.includes("down")){
+      //     imgElement.src = src.replace("down", "up")
+      // }else{
+      //     imgElement.src = src.replace("up", "down")
+      // }
+    });
+  });
+});
+
+function menuBar() {
+  let open = document.getElementById("open");
+  let closed = document.getElementById("closed");
+  let menu = document.getElementById("menu");
+
+  open.classList.toggle("open");
+  closed.classList.toggle("closed");
+  menu.classList.toggle("ctn");
 }
